7b3642c40a9ec0638e8b53f393dfc1b60ab21ba6
Smida taher <med.t.smida@gmail.com>
2024-12-20 10:54:14 +0100

    create the project

---
+++ Part2/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.part2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.part2 to javafx.fxml;
+    exports com.example.part2;
+}
